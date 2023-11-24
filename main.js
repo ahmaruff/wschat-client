3ffd8e9a7bcc61bb2841d784c500ae3f1a9b1594
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-11-25 00:01:19 +0700

    feat: chat UI

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import Alpine from 'alpinejs'
-import router from './src/router'
-window.alpine = Alpine
+import router from './src/router';
+
+window.Alpine = Alpine;
+
 Alpine.data('router', router)
-Alpine.start()
+
+Alpine.start();
--- src/router.js
+++ src/router.js
@@ -1,40 +1,49 @@
 export default () => {
   return {
-    route: window.location.hash || '#/',
-    isLoggedIn: false, // Menandakan status login
-    username: '',
-    password: '',
-
-    changeRoute(newRoute) {
-      if (newRoute === '#/chat' && !this.isLoggedIn) {
-        this.route = '#/';
-        alert('Please login to access the chat room.');
-      } else {
-        this.route = newRoute;
-        window.location.hash = newRoute;
-      }
+    username : 'username33',
+    currentRoute : window.location.hash || '#/',
+    navigate(route) {
+      this.currentRoute = route;
+      window.location.hash = route;
     },
-
-    async login() {
-      // Simulasi request ke server untuk mendapatkan JWT
-      const response = await fetch('https://your-auth-api.com/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username: this.username, password: this.password }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        // Simpan token JWT setelah login berhasil
-        localStorage.setItem('token', data.token);
-        this.isLoggedIn = true;
-        alert('Login successful!');
-      } else {
-        alert('Login failed. Please check your credentials.');
-      }
-    }
   }
-
 }
+
+// export default () => ({
+//   return () => {
+//     route: window.location.hash || '#/',
+//       isLoggedIn: false, // Menandakan status login
+//       changeRoute(newRoute) {
+//         this.route = newRoute;
+//         window.location.hash = newRoute;
+//         // if (newRoute === '#/chat' && !this.isLoggedIn) {
+//         //   this.route = '#/';
+//         //   alert('Please login to access the chat room.');
+//         // } else {
+//         //   this.route = newRoute;
+//         //   window.location.hash = newRoute;
+//         // }
+//       },
+//       async login() {
+//         // Simulasi request ke server untuk mendapatkan JWT
+//         const response = await fetch('https://your-auth-api.com/login', {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify({ username: this.username, password: this.password }),
+//         });
+  
+//         if (response.ok) {
+//           const data = await response.json();
+//           // Simpan token JWT setelah login berhasil
+//           localStorage.setItem('token', data.token);
+//           this.isLoggedIn = true;
+//           alert('Login successful!');
+//         } else {
+//           alert('Login failed. Please check your credentials.');
+//         }
+//       }
+//     }
+//   }  
+// })
